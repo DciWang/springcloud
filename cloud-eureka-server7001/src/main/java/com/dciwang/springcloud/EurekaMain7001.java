9bc9694adc391f709306130a7bfdb53617d3be2d
wangduocong <dciwang@163.com>
2020-08-05 15:19:01 +0800

    add eureka

---
+++ cloud-eureka-server7001/src/main/java/com/dciwang/springcloud/EurekaMain7001.java
@@ -0,0 +1,20 @@
+package com.dciwang.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @Author DciWang
+ * @Description TODO
+ * @Date 2020/8/5 下午2:24
+ * @Version 1.0
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaMain7001 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7001.class,args);
+    }
+
+}
